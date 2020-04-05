e78ac0bdd7342a269bd0bd0fb3151c46b1a23c13
Baynov Ivan <Jhon0@yandex.ru>
2020-04-05 16:41:33 +0500

    added routing to TopAppBar

--- src/containers/TopAppBar.jsx
+++ src/containers/TopAppBar.jsx
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 
 import { Link as RouterLink } from 'react-router-dom';
 import {
-  Typography, Toolbar, AppBar, Grid,
+  Typography,
+  Toolbar,
+  AppBar,
+  Grid,
+  Button,
 } from '@material-ui/core';
+import InputIcon from '@material-ui/icons/Input';
 import { white } from 'material-ui/styles/colors';
 
 class TopAppBar extends Component {
@@ -12,21 +17,34 @@ class TopAppBar extends Component {
     return (
       <AppBar position="static" style={{ marginBottom: '20px' }}>
         <Toolbar>
-          <Grid container direction="row" alignItems="flex-end" containerjustify="left" spacing={4}>
+
+          <Grid container direction="row" alignItems="flex-end" justify="space-between" spacing={4}>
             <Grid item>
               <RouterLink to="/">
                 <Typography variant="h2" style={{ color: white }}>Title</Typography>
               </RouterLink>
             </Grid>
-            {/* <Grid item>
-              <RouterLink to="/projects">
-                <Typography variant="h3" style={{ color: white }}>Проекты</Typography>
-              </RouterLink>
-            </Grid> */}
             <Grid item>
-              {/* <RouterLink to={`profile/${this.props.currentUser.id}`}>
-                <Typography variant="h2" style={{ color: white }}>Профиль</Typography>
-              </RouterLink> */}
+              <Grid container direction="row" containerjustify="left" spacing={4}>
+                <Grid item>
+                  <RouterLink to="/projects">
+                    <Typography variant="h3" style={{ color: white }}>Проекты</Typography>
+                  </RouterLink>
+                </Grid>
+                <Grid item>
+                  <RouterLink to={`profile/${this.props.currentUser.id}`}>
+                    <Typography variant="h2" style={{ color: white }}>Профиль</Typography>
+                  </RouterLink>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Button
+                color="inherit"
+              >
+                <InputIcon style={{ marginRight: '5px' }} />
+                Sign out
+              </Button>
             </Grid>
           </Grid>
         </Toolbar>
